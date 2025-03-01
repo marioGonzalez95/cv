b80576dcfa819d855b9766d52f44fa45ff83e38c
Mario <mariog.contacto@gmail.com>
2025-03-01 15:27:50 -0300

    pagina en contruccion

--- assets/js/header.js
+++
@@ -1,6 +0,0 @@
-fetch("components/header.html") // Ruta del archivo HTML
-  .then((response) => response.text()) // Convertir la respuesta a texto
-  .then((data) => {
-    document.getElementById("header").innerHTML = data; // Insertar el contenido
-  })
-  .catch((error) => console.error("Error al cargar el archivo:", error));
